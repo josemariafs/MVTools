4cb7d9e34af1636a077ebb4dd6ceef50edc8ab7f
Alejandro Suárez <alex.snako@hotmail.com>
2025-03-09 16:21:17 +0100

    refactor: simplify config service methods for premium and ultra-wide settings

--- src/services/config-service.ts
+++ src/services/config-service.ts
@@ -2,23 +2,28 @@ import { getStoredProperty, setStoredProperty } from '@/services/storage-service
 
 import { STORAGE_KEYS } from '../constants'
 
+const getIsPremiumEnabled = () => getStoredProperty(STORAGE_KEYS.MV_PREMIUM_ENABLED, false)
+const setPremiumEnabled = (value: boolean) => setStoredProperty(STORAGE_KEYS.MV_PREMIUM_ENABLED, value)
+const getIsPremiumBackgroundDisabled = () => getStoredProperty(STORAGE_KEYS.MV_PREMIUM_BG_DISABLED, false)
+const setPremiumBackgroundDisabled = (value: boolean) => setStoredProperty(STORAGE_KEYS.MV_PREMIUM_BG_DISABLED, value)
+const getIsUltraWideEnabled = () => getStoredProperty(STORAGE_KEYS.MV_ULTRA_WIDE_ENABLED, false)
+const setUltraWideEnabled = (value: boolean) => setStoredProperty(STORAGE_KEYS.MV_ULTRA_WIDE_ENABLED, value)
+
 export const getConfigService = () => ({
-  getIsPremiumEnabled: () => getStoredProperty(STORAGE_KEYS.MV_PREMIUM_ENABLED, false),
-  setPremiumEnabled: (value: boolean) => setStoredProperty(STORAGE_KEYS.MV_PREMIUM_ENABLED, value),
-  getIsPremiumBackgroundDisabled: () => getStoredProperty(STORAGE_KEYS.MV_PREMIUM_BG_DISABLED, false),
-  setPremiumBackgroundDisabled: (value: boolean) => setStoredProperty(STORAGE_KEYS.MV_PREMIUM_BG_DISABLED, value),
-  getIsUltraWideEnabled: () => getStoredProperty(STORAGE_KEYS.MV_ULTRA_WIDE_ENABLED, false),
-  setUltraWideEnabled: (value: boolean) => setStoredProperty(STORAGE_KEYS.MV_ULTRA_WIDE_ENABLED, value),
+  getIsPremiumEnabled,
+  setPremiumEnabled,
+  getIsPremiumBackgroundDisabled,
+  setPremiumBackgroundDisabled,
+  getIsUltraWideEnabled,
+  setUltraWideEnabled,
   getAll: () =>
-    Promise.all([
-      getStoredProperty(STORAGE_KEYS.MV_PREMIUM_ENABLED, false),
-      getStoredProperty(STORAGE_KEYS.MV_PREMIUM_BG_DISABLED, false),
-      getStoredProperty(STORAGE_KEYS.MV_ULTRA_WIDE_ENABLED, false)
-    ]).then(([premiumEnabled, premiumBgDisabled, ultraWideEnabled]) => {
-      return {
-        [STORAGE_KEYS.MV_PREMIUM_ENABLED]: premiumEnabled,
-        [STORAGE_KEYS.MV_PREMIUM_BG_DISABLED]: premiumBgDisabled,
-        [STORAGE_KEYS.MV_ULTRA_WIDE_ENABLED]: ultraWideEnabled
+    Promise.all([getIsPremiumEnabled, getIsPremiumBackgroundDisabled, getIsUltraWideEnabled]).then(
+      ([premiumEnabled, premiumBgDisabled, ultraWideEnabled]) => {
+        return {
+          [STORAGE_KEYS.MV_PREMIUM_ENABLED]: premiumEnabled,
+          [STORAGE_KEYS.MV_PREMIUM_BG_DISABLED]: premiumBgDisabled,
+          [STORAGE_KEYS.MV_ULTRA_WIDE_ENABLED]: ultraWideEnabled
+        }
       }
-    })
+    )
 })
