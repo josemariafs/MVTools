071936c2ab3e5126f40f8e9a40504be737fe6b9e
Alejandro Suárez <alex.snako@hotmail.com>
2025-03-15 02:18:29 +0100

    fix: update main element height for better responsiveness and remove unnecessary classes from HTML

--- src/entries/popup/App.tsx
+++ src/entries/popup/App.tsx
@@ -4,7 +4,7 @@ import { StylesSection } from '@/entries/popup/components/sections/styles'
 
 const App = () => {
   return (
-    <main className='min-h-maxPopupHeight relative h-max min-w-[500px] border-x-4 border-x-orange-500 p-7'>
+    <main className='min-h-maxPopupHeight relative h-screen min-w-[500px] overflow-auto border-x-4 border-x-orange-500 p-7'>
       <NewPopupButton />
       <div className='space-y-5'>
         <StylesSection />
