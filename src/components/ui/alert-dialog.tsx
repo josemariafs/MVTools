4549f68487e7bba6d483b8eaa33aac3655f6df37
Alejandro Suárez <alex.snako@hotmail.com>
2025-04-05 13:40:27 +0200

    fix: fix accessibility errors for alert and dialog components when using in shadow dom

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -31,8 +31,6 @@ const AlertDialogContent = React.forwardRef<
   <AlertDialogPortal container={container}>
     <AlertDialogOverlay />
     <AlertDialogPrimitive.Content
-      aria-labelledby='title'
-      aria-describedby='content'
       ref={ref}
       className={cn(
         'fixed left-[50%] top-[50%] z-[110] grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
@@ -65,7 +63,6 @@ const AlertDialogTitle = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Title>
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Title
-    id='title'
     ref={ref}
     className={cn('text-lg font-semibold', className)}
     {...props}
@@ -78,7 +75,6 @@ const AlertDialogDescription = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Description>
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Description
-    id='content'
     ref={ref}
     className={cn('text-sm text-muted-foreground', className)}
     {...props}
