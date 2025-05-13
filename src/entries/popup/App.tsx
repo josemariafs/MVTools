76fb5ca0f44534e6fa84e4bfcec2eab668b9452c
Aleksnako <alex.snako@hotmail.com>
2025-05-13 11:16:28 +0200

    refactor(popup): extract and reuse useCurrentBrowserWindow hook
    - Introduced `useCurrentBrowserWindow` hook to manage browser window type logic.
    - Updated `App` and `NewPopupButton` components to utilize the new hook, improving code reusability and readability.

--- src/entries/popup/App.tsx
+++ src/entries/popup/App.tsx
@@ -5,12 +5,20 @@ import { NewPopupButton } from '@/entries/popup/components/new-popup-button'
 import { MigrateFromLocalStoragePanel } from '@/entries/popup/components/panels/migrate-from-local-storage'
 import { GlobalConfigSection } from '@/entries/popup/components/sections/global-config'
 import { StylesSection } from '@/entries/popup/components/sections/styles'
+import { useCurrentBrowserWindow } from '@/entries/popup/hooks/use-current-browser-window'
+import { cn } from '@/utils/tailwind'
 
 const App = () => {
+  const { isPopup } = useCurrentBrowserWindow()
   useStorageListeners()
 
   return (
-    <main className='min-h-maxPopupHeight relative h-screen min-w-[500px] overflow-auto border-x-4 border-x-orange-500 p-6'>
+    <main
+      className={cn(
+        'min-h-maxPopupHeight relative h-screen min-w-[500px] overflow-auto border-x-4 border-x-orange-500 p-6',
+        !isPopup && 'max-w-[500px]'
+      )}
+    >
       <Suspense>
         <MigrateFromLocalStoragePanel />
       </Suspense>
